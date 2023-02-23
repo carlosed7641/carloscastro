d12854d37e83921ba00ca3f512cc2c6c33732567
Carlos Castro <carlosed7641@gmail.com>
2023-02-22 21:42:17 -0500

    feat: presentación

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -5,17 +5,17 @@ export const Presentation = () => {
         <section className='presentation-container container'>
             <div className='presentation-info'>
                 <h1 className='presentation-primary-text'>Carlos Castro</h1>
-                <h2 className='presentation-secondary-text'>Frontend Developer &</h2>
-                <h2 className='presentation-secondary-text'>Ingeniero de Sistemas</h2>
+                <h2 className='presentation-secondary-text'>Ingeniero de Sistemas  &</h2>
+                <h2 className='presentation-secondary-text'>Frontend Developer</h2>
                 <p>
-                    Apasionado por encontrar soluciones a través de código
+                    Apasionado por encontrar soluciones a través de código. <br />
+                    Atraverse es hacer!
                 </p>
+                <button className='curriculum'>Currículum</button>
             </div>
             <div className='presentation-image'>
-                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" 
-                width={240}
-                height={240}
-                alt="Imagen" />
+                <div className='overlay-icons'></div>
+                <img src="https://i.ibb.co/GxM5Xcg/IMG-20230208-172556.jpg" alt="Imagen" />
             </div>
         </section>
     )
